aba2db12a257a1fb5d11f496abaf08959a66a520
KsonaRR <dd10091980@gmail.com>
2024-01-20 21:04:52 +0300

    3

--- app/src/main/java/com/example/jirafamily/FirstPageActivity.kt
+++ app/src/main/java/com/example/jirafamily/FirstPageActivity.kt
@@ -1,11 +1,54 @@
 package com.example.jirafamily
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class FirstPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_page)
+
+        var loginButton:Button = findViewById(R.id.LoginButton)
+        var registrationButton:Button = findViewById(R.id.RegistrationButton)
+        var tokenButton:Button = findViewById(R.id.TokenButton)
+
+        loginButton.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        })
+
+        registrationButton.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        })
+
+        tokenButton.setOnClickListener {
+            showTokenInputDialog()
+        }
+    }
+
+    private fun showTokenInputDialog() {
+        val builder = AlertDialog.Builder(this)
+        val inflater = LayoutInflater.from(this)
+        val dialogView = inflater.inflate(R.layout.dialog_input_token, null)
+        builder.setView(dialogView)
+
+        val editTextToken = dialogView.findViewById<EditText>(R.id.editTextToken)
+        val btnContinue = dialogView.findViewById<Button>(R.id.btnContinue)
+
+        val dialog = builder.create()
+        dialog.show()
+
+        btnContinue.setOnClickListener {
+            val enteredToken = editTextToken.text.toString()
+
+            // TODO: Добавьте обработку введенного токена
+
+            dialog.dismiss()
+        }
     }
 }
--- app/src/main/java/com/example/jirafamily/MainActivity.kt
+++ app/src/main/java/com/example/jirafamily/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.example.jirafamily
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
-    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val durationMilliseconds = 5000L
-        handler.postDelayed({
-        }, durationMilliseconds)
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        handler.removeCallbacksAndMessages(null)
+        setContentView(R.layout.activity_loader)
+
+        Handler().postDelayed({
+            val intent = Intent(this, FirstPageActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
